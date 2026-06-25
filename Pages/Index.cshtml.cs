bf00bcdec594b70c86bba29ee4017835778b0190
Nick Milburn <nickmilburn@proton.me>
2026-06-25 14:12:25 -0400

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace erefugia.art.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
